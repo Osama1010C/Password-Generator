002f93f17440203e338dc9d1b635af59de2bafe7
OSaMa <166417491+Osama1010C@users.noreply.github.com>
2024-07-19 00:39:01 +0300

    Add files via upload

---
+++ Password Genrator/Password Genrator/Generator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Genrator
+{
+    class Generator
+    {
+        private static char[] easyChars = { '1', '2', '3','4','5','6','7','8','9','0','a',
+                                'b','c','q','w','e','r','t','y','u','i','o','p'
+                                ,'s','d','f','g','h','j','k','l','z','x','c','v'
+                                ,'n','m'};
+        private static char[] hardChars = { '1', '2', '3','4','5','6','7','8','9','0','a',
+                                'b','c','q','w','e','r','t','y','u','i','o','p'
+                                ,'s','d','f','g','h','j','k','l','z','x','c','v'
+                                ,'n','m','Q','W','E','R','T','Y','U','I','O','P'
+                                ,'A','S','D','F','G','H','J','K','L','Z','X','C'
+                                ,'V','B','N','M','!','@','#','$','%','^','&','*','(',
+                                ')','-','_','=','+','{','}','[',']',',','.','?'
+                                ,'/','`','~',';',':'};
+        public static String[] SmallPassword()
+        {
+            Random r = new Random();
+            String[] pass = { "", "" };
+            for (int i = 0; i < 8; i++)
+            {
+                int num = r.Next(0, easyChars.Length);
+                int n = num;
+                pass[0] += easyChars[n];
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                int num = r.Next(0, hardChars.Length);
+                int n = num;
+                pass[1] += hardChars[n];
+            }
+
+            return pass;
+        }
+
+        public static String[] MediumPassword()
+        {
+            Random r = new Random();
+            String[] pass = { "", "" };
+            for (int i = 0; i < 15; i++)
+            {
+                int num = r.Next(0, easyChars.Length);
+                int n = num;
+                pass[0] += easyChars[n];
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                int num = r.Next(0, hardChars.Length);
+                int n = num;
+                pass[1] += hardChars[n];
+            }
+
+            return pass;
+        }
+
+        public static String[] HardPassword()
+        {
+            Random r = new Random();
+            String[] pass = { "", "" };
+            for (int i = 0; i < 25; i++)
+            {
+                int num = r.Next(0, easyChars.Length);
+                int n = num;
+                pass[0] += easyChars[n];
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                int num = r.Next(0, hardChars.Length);
+                int n = num;
+                pass[1] += hardChars[n];
+            }
+
+            return pass;
+        }
+    }
+}
---
+++ Password Genrator/Password Genrator/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Password_Genrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+        }
+        public static void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("========================\n   Password Generator\n========================\n");
+                Console.WriteLine("1] Generate Small Password\n2] Generate Medium Password\n3] Generate Large Password\n4] Exit ");
+                int choice = 0;
+                while (true)
+                {
+                    try
+                    {
+                        choice = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                    if (choice > 4 || choice < 1)
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                    else break;
+                }
+                String[] pass = { "", "" };
+                if(choice == 1)
+                {
+                    Console.Clear();
+
+                    pass = Generator.SmallPassword();
+
+                    Console.WriteLine($"Normal Password : {pass[0]}\n");
+                    Console.WriteLine($"Hard Password : {pass[1]}\n");
+
+                    Console.WriteLine("< Click enter to back >");
+                    String back = Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if(choice == 2)
+                {
+                    Console.Clear();
+
+                    pass = Generator.MediumPassword();
+
+                    Console.WriteLine($"Normal Password : {pass[0]}\n");
+                    Console.WriteLine($"Hard Password : {pass[1]}\n");
+
+                    Console.WriteLine("< Click enter to back >");
+                    String back = Console.ReadLine();
+
+                    Console.Clear();
+
+                }
+                else if (choice == 3)
+                {
+                    Console.Clear();
+
+                    pass = Generator.HardPassword();
+
+                    Console.WriteLine($"Normal Password : {pass[0]}\n");
+                    Console.WriteLine($"Hard Password : {pass[1]}\n");
+
+                    Console.WriteLine("< Click enter to back >");
+                    String back = Console.ReadLine();
+
+                    Console.Clear();
+
+                }
+                else
+                {
+                    Console.Clear();
+                    break;
+                }
+            }
+        }
+    }
+}
